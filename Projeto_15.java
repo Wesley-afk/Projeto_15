0203005f2a3e2918760e55f15e84e406e966881b
Wesley Pedreira Passos Rocha <wesley.p.rocha6@aluno.senai.br>
2024-09-25 21:20:56 -0300

    Add files via upload

---
+++ Projeto_15.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_15;
+
+/**
+ *
+ * @author w.rocha
+ */
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.io.IOException;
+public class Projeto_15 {
+
+    public static void main(String[] args) throws IOException {
+        Scanner ler = new Scanner (System.in);
+        int vetor[]; vetor = new int[5];
+        String []nome_do_arquivo; nome_do_arquivo = new String[5];
+        
+        System.out.println("Digite 5 valores.\n"); //pede para o usuário digitar 5 valores para a multiplicação
+        for (int i = 0; i < 5; i++) {
+            vetor[i] = ler.nextInt();
+        }
+        
+        System.out.println("Digite o nome dos aqruivos. \n");
+        for(int i = 0; i < 5; i++){
+            nome_do_arquivo[i] = ler.nextLine();
+        }
+           
+        for(int i = 0; i < 5; i++){
+        FileWriter arquivo = new FileWriter("C:\\Users\\w.rocha\\Documents\\TxT" + nome_do_arquivo[i] + ".txt");
+        PrintWriter gravar = new PrintWriter(nome_do_arquivo[i]);
+        }
+    }
+}        
